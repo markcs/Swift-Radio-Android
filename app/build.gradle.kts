41e4be7389d18d64fe74c354b0860ea2ac43bb61
Fethi El Hassasna <e.fethi.c@gmail.com>
2026-02-14 15:30:11 -0500

    Add Coil 3 dependencies for Compose image loading (3.3.0)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,8 @@ dependencies {
     implementation(libs.ktor.client.okhttp)
     implementation(libs.ktor.client.content.negotiation)
     implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.coil.compose)
+    implementation(libs.coil.network.okhttp)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
