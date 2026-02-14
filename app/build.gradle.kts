d85bfcb6accc9ed055865c81c4bddf5a31ec2ac9
Fethi El Hassasna <e.fethi.c@gmail.com>
2026-02-14 12:59:42 -0500

    Add media3-ui-compose-material3 dependency (1.9.2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.media3.exoplayer)
     implementation(libs.androidx.media3.session)
+    implementation(libs.androidx.media3.ui.compose.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
